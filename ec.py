6d08a6008b6c789602cbc714b2d39e27c52b8ce4
bossacode <hlee2745@gmail.com>
2024-01-24 02:07:07 +0900

    modules using euler characteristic

--- ec.py
+++ ec.py
@@ -2,30 +2,110 @@ import torch
 import torch.nn as nn
 import numpy as np
 import gudhi
+from torch_topological.nn import CubicalComplex
 
 
-class EC_CustomGrad(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, input, tseq, grid_size):
+# class EC_CustomGrad(torch.autograd.Function):
+#     @staticmethod
+#     def forward(ctx, input, tseq, grid_size):
+#         """
+#         Args:
+#             input: Tensor of shape [(C*H*W),]
+#             tseq:
+#             grid_size:
+#         Returns:
+#         """
+#         cub_cpx = gudhi.CubicalComplex(dimensions=grid_size, top_dimensional_cells=input)
+#         cub_cpx.compute_persistence(homology_coeff_field=2, min_persistence=0)
+#         ec = torch.zeros(len(tseq))
+#         for i,t in enumerate(tseq):
+#             betti_num = cub_cpx.persistent_betti_numbers(t,t)
+#             ec[i] = betti_num[0] - betti_num[1]
+#         return ec
+    
+#     @staticmethod
+#     def backward(ctx, up_grad):
+#         pass
+
+    
+class EC_Layer(nn.Module):
+    def __init__(self, T=50, num_channels=3):
+        super().__init__()
+        self.cub_cpx = CubicalComplex(superlevel=False, dim=2)
+        self.T = T
+        self.tseq = torch.linspace(0, 1, T).view(1, -1) # shape: [1, T]
+        self.num_channels = num_channels
+
+    def forward(self, input):
         """
         Args:
-            input: Tensor of shape [(C*H*W),]
-            tseq:
-            grid_size:
+            input: Tensor of shape [batch_size, C, H, W]
         Returns:
+            ec: Tensor of shape [batch_size, C, T]
         """
-        cub_cpx = gudhi.CubicalComplex(dimensions=grid_size, top_dimensional_cells=input)
-        cub_cpx.compute_persistence(homology_coeff_field=2, min_persistence=0)
-        ec = torch.zeros(len(tseq))
-        for i,t in enumerate(tseq):
-            betti_num = cub_cpx.persistent_betti_numbers(t,t)
-            ec[i] = betti_num[0] - betti_num[1]
+        batch_size = input.shape[0]
+        
+        pi = self.cub_cpx(input)
+        ec = torch.zeros(batch_size, self.num_channels, self.T)
+        
+        for b in range(batch_size):
+            for c in range(self.num_channels):
+                pd_0 = pi[b][c][0].diagram
+                pd_1 = pi[b][c][1].diagram
+                betti_0 = torch.logical_and(pd_0[:, 0].view(-1, 1) < self.tseq, pd_0[:, 1].view(-1, 1) >= self.tseq).sum(dim=0)
+                betti_1 = torch.logical_and(pd_1[:, 0].view(-1, 1) < self.tseq, pd_1[:, 1].view(-1, 1) >= self.tseq).sum(dim=0)
+                ec[b, c, :] = betti_0 - betti_1
         return ec
     
-    @staticmethod
-    def backward(ctx, up_grad):
-        pass
 
-class EC_layer(nn.Module):
-    def __init__(self):
+# class EC_Layer2(nn.Module):
+#     def __init__(self, T=50):
+#         super().__init__()
+#         self.tseq = torch.linspace(0, 1, T)
+
+#     def forward(self, input, grid_size):
+#         """
+#         Args:
+#             input: Tensor of shape [(C*H*W),]
+#             tseq:
+#             grid_size:
+#         Returns:
+#         """
+#         cub_cpx = gudhi.CubicalComplex(dimensions=grid_size, top_dimensional_cells=input)
+#         cub_cpx.compute_persistence(homology_coeff_field=2, min_persistence=0)
+#         ec = torch.zeros(len(self.tseq))
+#         for i, t in enumerate(self.tseq):
+#             betti_num = cub_cpx.persistent_betti_numbers(t,t)
+#             ec[i] = betti_num[0] - betti_num[1]
+#         return ec
+    
+
+class EC_TopoLayer(nn.Module):
+    def __init__(self, T=50, num_channels=3, out_features=100, p=0):
+        """
+        Args:
+            T: 
+            K_max: 
+            dimensions: 
+            num_channels: number of channels in input
+            out_features: output dimension of fc layer
+            p: dropout probability
+        """
         super().__init__()
+        self.ec_layer = EC_Layer(T, num_channels)
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(p)
+        self.gtheta_layer = nn.Linear(num_channels * T, out_features)
+
+    def forward(self, input):
+        """
+        Args:
+            input: Tensor of shape [batch_size, C, H, W]
+
+        Returns:
+            output: Tensor of shape [batch_size, out_features]
+        """
+        ec = self.ec_layer(input)
+        ec = self.dropout(self.flatten(ec)) # shape: [batch_size, (num_channels * T)]
+        output = self.gtheta_layer(ec)
+        return output
---
+++ train_ecpllay.py
@@ -0,0 +1,88 @@
+import torch
+import os
+import wandb
+import argparse
+from models_base import EC_Pllay
+from train_test import train_pipeline_wandb, _train_pipeline
+
+
+# for reproducibility (may degrade performance)
+torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.deterministic = True
+torch.manual_seed(123)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("data", help="directory of the dataset")
+args = parser.parse_args()
+
+project = "EC_Pllay" + args.data
+
+# hyperparams, model params, metadata, etc.
+config = {
+    "batch_size": 32,
+    "lr": 0.01,
+    "weight_decay":0.0001,
+    "factor": 0.3,          # factor to decay lr by when loss stagnates
+    "threshold": 0.005,     # min value to be considered as improvement in loss
+    # "es_patience": 25,      # earlystopping patience
+    "sch_patience": 15,     # lr scheduler patience
+    "epochs": 500,
+    "val_size": 0.3,
+    "ntimes": 1,            # number of repetitions for simulation of each model
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+    "model_params": dict(
+        num_classes=10,
+        use_dtm=False,
+        # DTM parameters
+        # m0=0.05,
+        # lims=[[1,28], [1,28]],
+        # size=[28, 28],
+        # r=2,
+        # PL parameters
+        T=50,
+        num_channels=3,
+        out_features=50,
+        p=0.3
+        ),
+    "data": args.data,
+    "model": "EC_Pllay"
+    }
+
+# corrupt_prob_list = [0.0, 0.1, 0.2]
+# noise_prob_list = [0.0, 0.1, 0.2]
+corrupt_prob_list = [0.0]
+noise_prob_list = [0.0]
+len_cn = len(corrupt_prob_list)
+file_cn_list, weight_dir_list = [], []
+for i_cn in range(len_cn):
+    file_cn_list.append(str(int(corrupt_prob_list[i_cn] * 100)).zfill(2) + "_" + str(int(noise_prob_list[i_cn] * 100)).zfill(2))
+    weight_dir = f"{args.data}/saved_weights/{project}/{file_cn_list[i_cn]}"  # directory path to store trained model weights
+    if not os.path.exists(weight_dir):
+        os.makedirs(weight_dir)
+    weight_dir_list.append(weight_dir)
+x_path_list = [f"{args.data}/generated_data/x_" + file_cn_list[i] + ".pt" for i in range(len_cn)]
+y_path = f"{args.data}/generated_data/y.pt"
+
+
+if __name__ == "__main__":
+    wandb.login()
+
+    # train
+    # loop over data with different corruption/noise probability
+    for i_cn in range(len_cn):
+        print("-"*30)
+        print(f"Corruption/Noise rate: {file_cn_list[i_cn]}")
+        print("-"*30)
+        
+        # loop over number of simulations
+        for n_sim in range(1, config["ntimes"]+1):
+            print(f"\nSimulation: [{n_sim} / {config['ntimes']}]")
+            print("-"*30)
+            
+            group = file_cn_list[i_cn]                              # used for grouping experiments in wandb
+            job_type = f"sim{n_sim}"                                # used for specifying runs in wandb
+            # weight_path = weight_dir_list[i_cn] + f"sim{n_sim}.pt"  # file path to save trained weights
+            weight_path = None
+            seed = torch.randint(0,1000, size=(1,)).item()          # used for different train/val split in each simulataion
+            train_pipeline_wandb(EC_Pllay, config, x_path_list[i_cn], y_path, seed, weight_path, project, group, job_type)
+            # _train_pipeline(ScaledPllay, config, x_path_list[i_cn], y_path, seed, weight_path)
