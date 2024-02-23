1b44f9870cc593b7a4da187401843b7e1018e212
bossacode <hlee2745@gmail.com>
2024-02-23 21:31:53 +0900

    parser takes additional arguments block_cfg and filter_cfg

--- hp_resnet.py
+++ hp_resnet.py
@@ -3,9 +3,10 @@ import wandb
 import argparse
 import os
 from functools import partial
-from models import ResNet18_16
-from train_test import train_pipeline_wandb
+from models import ResNet
+from train_test import train_val_wandb
 
+# python hp_resnet.py -d MNIST -b 2 2 2 -f 16 32 64
 
 # for reproducibility (may degrade performance)
 torch.backends.cudnn.benchmark = False
@@ -13,37 +14,39 @@ torch.backends.cudnn.deterministic = True
 torch.manual_seed(123)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("data", help="directory of the dataset")
+# parser.add_argument("data", help="directory of the dataset")
+parser.add_argument("-d", "--data", help="directory of the dataset")
+parser.add_argument("-b", "--block_cfg", nargs="+", type=int, help="block configuration of ResNet")
+parser.add_argument("-f", "--filter_cfg", nargs="+", type=int, help="filter configuration of ResNet")
 args = parser.parse_args()
 
-project = "hp_ResNest18_16_300" + args.data
+project = "hp_ResNet"
 
 sweep_config = {
     "method": "random",
-    "metric": {"name": "val.loss", "goal": "minimize"},
+    "metric": {"name": "best_val.loss", "goal": "minimize"},
     "parameters": {
-        "batch_size": {"values": [8, 16, 32, 64]},
+        "batch_size": {"value": 16},
         "device": {"value": "cuda" if torch.cuda.is_available() else "cpu"},
-        "epochs": {"value": 500},
+        "epochs": {"value": 200},
         "factor":{"values": [0.1, 0.2, 0.3, 0.4, 0.5]},
-        "lr": {"distribution": "inv_log_uniform_values", "min": 0.001, "max": 0.1},
+        "lr": {"distribution": "inv_log_uniform_values", "min": 0.001, "max": 0.05},
         "model_params": {
             "parameters": dict(
                 in_channels={"value": 1},
                 # block=ResidualBlock,
-                # cfg=[2,2,2,2],
+                block_cfg={"value": args.block_cfg},
+                filter_cfg={"value": args.filter_cfg},
                 num_classes={"value": 10},
-                res_in_channels={"value": 16},
                 p={"values": [0.1, 0.2, 0.3, 0.4, 0.5]}
             )
         },
         # "es_patience": {"value": 35},
-        "sch_patience": {"values": [5, 10, 15]},
-        "threshold": {"value": 0.005},
-        "val_size": {"value": 0.3},
-        "weight_decay": {"values": [0, 0.0001]},
+        "sch_patience": {"values": [10, 15]},
+        "threshold": {"value": 0.001},
+        "val_size": {"value": 0.8}
     },
-    "description": f"Hyperparameter search of ScaledPllay on {args.data} using 300 train data"
+    "description": f"Hyperparam search of ResNet on {args.data} using 100/400 train/val data"
 }
 
 
@@ -53,7 +56,7 @@ len_cn = len(corrupt_prob_list)
 file_cn_list, weight_dir_list = [], []
 for i_cn in range(len_cn):
     file_cn_list.append(str(int(corrupt_prob_list[i_cn] * 100)).zfill(2) + "_" + str(int(noise_prob_list[i_cn] * 100)).zfill(2))
-    weight_dir = f"{args.data}/saved_weights/{project}/{file_cn_list[i_cn]}"  # directory path to store trained model weights
+    weight_dir = f"{args.data}/saved_weights/{project}/{file_cn_list[i_cn]}/"  # directory path to store trained model weights
     if not os.path.exists(weight_dir):
         os.makedirs(weight_dir)
     weight_dir_list.append(weight_dir)
@@ -69,6 +72,6 @@ if __name__ == "__main__":
 
     # wandb.agent returns empty config if functions with arguments are given
     # https://github.com/wandb/wandb/issues/2724
-    train_pipeline = partial(train_pipeline_wandb, ResNet18_16, None, x_path_list[0], y_path, seed, project=project)
+    train_pipeline = partial(train_val_wandb, ResNet, None, x_path_list[0], y_path, seed, project=project)
     
-    wandb.agent(sweep_id, train_pipeline, project=project, count=300)
+    wandb.agent(sweep_id, train_pipeline, project=project, count=200)
