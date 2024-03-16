4b11b9d12560de0fd5c6840fa8c4149ab8cd74b3
bossacode <hlee2745@gmail.com>
2024-03-16 18:18:52 +0900

    bash script for running hp search

--- run_eclay_2layer.sh
+++ run_eclay_2layer.sh
@@ -7,6 +7,5 @@ pip install -r "requirements.txt"
 api_key="cb66d1605ac3cd7c944bbe89df3ca8c9bc6fe41e"
 wandb login $api_key
 
-python hp_pllay.py -d MNIST -t 128 64 32 -hid 32
-python hp_pllay.py -d MNIST -t 128 64 32 -hid 64
-python hp_pllay.py -d MNIST -t 128 64 -hid 128
+python hp_eclay.py -d MNIST -t 256 128 -hid 64 32
+python hp_eclay.py -d MNIST -t 512 256 -hid 128 64
