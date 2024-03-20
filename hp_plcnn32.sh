6d3f0787bcf07fc31882f0db9e47e98c59f83313
bossacode <hlee2745@gmail.com>
2024-03-20 22:49:11 +0900

    hp search for plcnn

---
+++ hp_plcnn32.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+cd /root/pllay/EClay/
+
+pip install -r "requirements.txt"
+
+api_key="cb66d1605ac3cd7c944bbe89df3ca8c9bc6fe41e"
+wandb login $api_key
+
+python hp_plcnn.py MNIST -hid 32
