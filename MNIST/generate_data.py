858ad63db50273693f749045fd7c9625d2009e31
bossacode <hlee2745@gmail.com>
2024-05-02 14:29:46 +0900

    Remove unused code blocks

--- MNIST/generate_data.py
+++ MNIST/generate_data.py
@@ -8,69 +8,6 @@ import numpy as np
 import os
 
 
-# def corrupt_noise(x, corrupt_p, noise_p):
-#     # corruption process
-#     corrupt_dist = Bernoulli(probs=corrupt_p)
-#     x_corrupt = torch.where(corrupt_dist.sample(x.shape).bool(),
-#                             0,
-#                             x)
-    
-#     # noise process
-#     noise_dist = Bernoulli(probs=noise_p)
-#     x_corrupt_noise = torch.where(((x < 0.01) * noise_dist.sample(x.shape)).bool(),
-#                                   torch.rand(x.shape),
-#                                   x_corrupt)
-#     return x_corrupt_noise
-
-
-# def generate_data(N, corrupt_prob_list, noise_prob_list, x_file_list, y_file, dir="./generated_data/"):
-#     # train data shape: (60000,28,28)
-#     train_data = MNIST(root="./raw_data",
-#                        train=True,
-#                        download=True,
-#                        transform=ToTensor())
-    
-#     # test data shape: (10000,28,28) 
-#     test_data = MNIST(root="./raw_data",
-#                       train=False,
-#                       download=True,
-#                       transform=ToTensor())
-    
-#     # normalize
-#     x_train = train_data.data / 255
-#     x_test = test_data.data / 255
-
-#     y_train = train_data.targets
-#     y_test = test_data.targets
-
-#     # sample N training data for each label
-#     idx_list = []
-#     for label in y_train.unique():
-#         sampled_idx = np.random.choice(torch.where(y_train == label)[0], N)
-#         idx_list.append(sampled_idx)
-#     idx = np.concatenate(idx_list)
-#     x_train = x_train[idx]
-#     y_train = y_train[idx]
-
-#     # ind = torch.randint(low=0, high=len(train_data.data), size=(N,))
-#     # x_train = x_train[ind]
-#     # y_train = y_train[ind]
-
-#     # add channel dimension: (N,C,H,W)
-#     x_train.unsqueeze_(1)
-#     x_test.unsqueeze_(1)
-
-#     if not os.path.exists(dir):
-#         os.makedirs(dir)
-#     torch.save((y_train, y_test), dir + y_file)
-
-#     len_cn = len(corrupt_prob_list)
-#     for i in range(len_cn):
-#         x_train_noise = corrupt_noise(x_train, corrupt_prob_list[i], noise_prob_list[i])
-#         x_test_noise = corrupt_noise(x_test, corrupt_prob_list[i], noise_prob_list[i])
-#         torch.save((x_train_noise, x_test_noise), dir + x_file_list[i])
-
-
 n_train = 10                   # number of training samples for each label
 n_val = int(n_train * 0.4)     # number of validation samples for each label
 
