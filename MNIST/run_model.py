24b27e0bee57c45d66462a638a34b5f0ccfa479b
bossacode <hlee2745@gmail.com>
2024-05-07 13:38:53 +0900

    Changed file name

--- MNIST/run_model.py
+++ MNIST/run_model.py
@@ -31,24 +31,23 @@ config["device"] = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 if __name__ == "__main__":
-    ntimes = 100  # number of simulations to run
-    noise_prob_list = [0.0]
+    ntimes = 50  # number of simulations to run
     # noise_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
-    n_train_list = [10]
-    # n_train_list = [10, 30, 50, 70, 100]    # list of number of training samples for each label
+    noise_prob_list = [0.0]
+    n_train_list = [100, 300, 500, 700, 1000]    # list of number of training samples for each label
+    # n_train_list = [100]
 
     wandb.login()
     for n_train in n_train_list:
-        project = f"MNIST{n_train*10}_" + args.model    # used as project name in wandb
+        project = f"NewMNIST{n_train}_" + args.model    # used as project name in wandb
         print("-"*30)
-        print(f"Number of training data: {n_train*10}")
+        print(f"Number of training data: {n_train}")
         print("-"*30)
         # loop over data with different noise probability
         for p in noise_prob_list:
             noise_prob = str(int(p * 100)).zfill(2)
-            weight_dir = f"saved_weights/data_{n_train*10}/{args.model}/{noise_prob}/"    # directory path to save trained weights
-            if not os.path.exists(weight_dir):
-                os.makedirs(weight_dir)
+            weight_dir = f"./saved_weights/data_{n_train}/{args.model}/{noise_prob}/"    # directory path to save trained weights
+            os.makedirs(weight_dir, exist_ok=True)
             
             print("-"*30)
             print(f"Noise rate: {noise_prob}")
@@ -59,7 +58,7 @@ if __name__ == "__main__":
                 print(f"\nSimulation: [{n_sim} / {ntimes}]")
                 print("-"*30)
                 
-                dir_path = f"generated_data/data_{n_train}/noise_{noise_prob}/" # directory path to data
+                dir_path = f"./dataset/processed/data_{n_train}/noise_{noise_prob}/" # directory path to data
                 weight_path = weight_dir + f"sim{n_sim}.pt"                     # file path to save trained weights
                 group = noise_prob                                              # used for grouping experiments in wandb
                 job_type = f"sim{n_sim}"                                        # used for specifying runs in wandb        
