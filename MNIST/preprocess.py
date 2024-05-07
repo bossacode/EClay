f540207d1e36a97ca8abff0e1ec9923e93dbacf8
bossacode <hlee2745@gmail.com>
2024-05-07 13:39:43 +0900

    n_train is the size of entire training datase. Before, it was size per label

--- MNIST/preprocess.py
+++ MNIST/preprocess.py
@@ -11,8 +11,7 @@ import os
 
 
 def add_noise(x, p):
-    """
-    Replace pixels with random noise between 0 and 1 with probabilty p.
+    """Replace pixels with random noise between 0 and 1 with probabilty p.
 
     Args:
         x (torch.Tensor): Original data.
@@ -29,8 +28,7 @@ def add_noise(x, p):
 
 
 def dtm_transform(x, dtm):
-    """
-    Tranform data using DTM.
+    """Tranform data using DTM.
 
     Args:
         x (torch.Tensor): Tensor of shape [B, C, H, W].
@@ -47,8 +45,7 @@ def dtm_transform(x, dtm):
 
 
 def ecc_transform(x, ecc):
-    """
-    Transform data to Euler Characteristic Curve.
+    """Transform data to Euler Characteristic Curve.
 
     Args:
         x (torch.Tensor): Tensor of shape [B, C, H, W].
@@ -65,8 +62,7 @@ def ecc_transform(x, ecc):
 
 
 def pl_transform(x, pl):
-    """
-    Transform data to Persistent Landscape.
+    """Transform data to Persistent Landscape.
 
     Args:
         x (torch.Tensor):  Tensor of shape [B, C, H, W].
@@ -90,8 +86,8 @@ def generate_data(n_train, n_val, noise_prob_list):
         n_val (int): Number of validation data to sample for each label.
         noise_prob_list (list): List containing noise probability.
     """
-    train_data = MNIST(root="./raw_data", train=True, download=True, transform=ToTensor())  # shape: (60000, 28, 28)
-    test_data = MNIST(root="./raw_data", train=False, download=True, transform=ToTensor())  # shape: (10000, 28, 28) 
+    train_data = MNIST(root="./dataset/raw/", train=True, download=True, transform=ToTensor())  # shape: (60000, 28, 28)
+    test_data = MNIST(root="./dataset/raw/", train=False, download=True, transform=ToTensor())  # shape: (10000, 28, 28)
     
     # normalize and add channel dimension: (N, C, H, W)
     x_train = (train_data.data / 255).unsqueeze(1)
@@ -99,32 +95,34 @@ def generate_data(n_train, n_val, noise_prob_list):
     x_test = (test_data.data / 255).unsqueeze(1)
     y_test = test_data.targets
 
-    # sample "n_train" training data and "n_val" validation data for each label
-    # total training data size is n_train * # of labels
+    # sample "n_train" training data and "n_val" validation data with equal proportion for each label
     train_idx_list = []
     val_idx_list = []
-    for label in y_train.unique():
-        idx = torch.where(y_train == label)[0]
-        sampled_idx = np.random.choice(idx, size=n_train + n_val, replace=False)
-        train_idx_list.append(sampled_idx[:n_train])
-        val_idx_list.append(sampled_idx[n_train:])
+    labels = y_train.unique()
+    n_train_per_label = int(n_train / len(labels))
+    n_val_per_label = int(n_val / len(labels))
+    for i in labels:
+        idx = torch.where(y_train == i)[0]
+        sampled_idx = np.random.choice(idx, size=n_train_per_label + n_val_per_label, replace=False)
+        train_idx_list.append(sampled_idx[:n_train_per_label])
+        val_idx_list.append(sampled_idx[n_train_per_label:])
     train_idx = np.concatenate(train_idx_list)
     val_idx = np.concatenate(val_idx_list)
     x_train_sampled, y_train_sampled = x_train[train_idx], y_train[train_idx]
     x_val_sampled, y_val_sampled = x_train[val_idx], y_train[val_idx]
 
-    dtm005 = DTMLayer(m0=0.05, lims=[[1, 28], [1, 28]], size=[28, 28])
-    ecc_dtm005 = CubECC2d(as_vertices=False, sublevel=True, size=[28, 28], interval=[0, 7, 32])
-    pl_dtm005 = CubPL2d(superlevel=False, tseq=[0, 7, 32], K_max=2, dimensions=[0, 1])
+    dtm005 = DTMLayer(m0=0.05, lims=[[-0.5, 0.5], [-0.5, 0.5]], size=[28, 28])
+    ecc_dtm005 = CubECC2d(as_vertices=False, sublevel=True, size=[28, 28], interval=[0.02, 0.28, 32])
+    pl_dtm005 = CubPL2d(superlevel=False, tseq=[0.02, 0.28, 32], K_max=2, dimensions=[0, 1])
 
-    dtm02 = DTMLayer(m0=0.2, lims=[[1, 28], [1, 28]], size=[28, 28])
-    ecc_dtm02 = CubECC2d(as_vertices=False, sublevel=True, size=[28, 28], interval=[1, 8, 32])
-    pl_dtm02 = CubPL2d(superlevel=False, tseq=[1, 8, 32], K_max=3, dimensions=[0, 1])
+    dtm02 = DTMLayer(m0=0.2, lims=[[-0.5, 0.5], [-0.5, 0.5]], size=[28, 28])
+    ecc_dtm02 = CubECC2d(as_vertices=False, sublevel=True, size=[28, 28], interval=[0.06, 0.29, 32])
+    pl_dtm02 = CubPL2d(superlevel=False, tseq=[0.06, 0.29, 32], K_max=3, dimensions=[0, 1])
 
     for p in noise_prob_list:
-        dir_name = f"generated_data/data_{n_train*10}/noise_" + str(int(p * 100)).zfill(2) + "/"
-        if not os.path.exists(dir_name):
-            os.makedirs(dir_name)
+        dir_name = f"./dataset/processed/data_{n_train}/noise_" + str(int(p * 100)).zfill(2) + "/"
+        os.makedirs(dir_name, exist_ok=True)
+        
         # train data
         x_train_noise = add_noise(x_train_sampled, p)
         x_train_dtm005 = dtm_transform(x_train_noise, dtm005)
@@ -158,11 +156,13 @@ def generate_data(n_train, n_val, noise_prob_list):
 
 
 if __name__ == "__main__":
-    noise_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25]
-    n_train_list = [10, 30, 50, 70, 100]    # number of training samples for each label
+    noise_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25] # noise probabilities
+    # noise_prob_list = [0.0]
+    n_train_list = [100, 300, 500, 700, 1000]           # number of training samples
+    # n_train_list = [100]
 
     torch.manual_seed(123)
     np.random.seed(123)
     for n_train in n_train_list:
-        n_val = int(n_train * 0.4)  # number of validation samples for each label
+        n_val = int(n_train * 0.4)  # number of validation samples
         generate_data(n_train, n_val, noise_prob_list)
