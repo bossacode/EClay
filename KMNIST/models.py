9fb94d69b28b1c7fedb085a8e4118194ad2d2821
bossacode <hlee2745@gmail.com>
2024-05-16 01:30:04 +0900

    Delete unused model

--- KMNIST/models.py
+++ KMNIST/models.py
@@ -23,7 +23,7 @@ class ResidualBlock(nn.Module):
         x = self.relu(x)
         x = self.conv_layer2(x)
         x = x + input if self.downsample is None else x + self.downsample(input)
-        # x = self.relu(x)
+        x = self.relu(x)
         return x
 
 
@@ -50,7 +50,6 @@ class ResNet18(nn.Module):
         self.fc = nn.Sequential(nn.Linear(filter_cfg[-1], 64),
                                 nn.ReLU(),
                                 nn.Linear(64, num_classes))
-        self.relu = nn.ReLU()
         # weight initialization
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -61,10 +60,10 @@ class ResNet18(nn.Module):
 
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
-        x = self.relu(self.res_layer_1(x))
-        x = self.relu(self.res_layer_2(x))
-        x = self.relu(self.res_layer_3(x))
-        x = self.relu(self.res_layer_4(x))
+        x = self.res_layer_1(x)
+        x = self.res_layer_2(x)
+        x = self.res_layer_3(x)
+        x = self.res_layer_4(x)
         x = self.avg_pool(x)
         output = self.fc(x)
         return output
@@ -89,7 +88,6 @@ class ResNet18(nn.Module):
         self.layer_input_channels = num_filters * block.expansion
         
         for _ in range(1, num_blocks):
-            block_list.append(nn.ReLU())
             block_list.append(block(self.layer_input_channels, num_filters))
         return nn.Sequential(*block_list)
 
@@ -108,10 +106,10 @@ class ECResNet(ResNet18):
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
         # ResNet
-        x = self.relu(self.res_layer_1(x))
-        x = self.relu(self.res_layer_2(x))
-        x = self.relu(self.res_layer_3(x))
-        x = self.relu(self.res_layer_4(x))
+        x = self.res_layer_1(x)
+        x = self.res_layer_2(x)
+        x = self.res_layer_3(x)
+        x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
 
         # EC Layer 1
@@ -127,7 +125,7 @@ class ECResNet(ResNet18):
 
 class ECResNet_Topo(ResNet18):
     def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,      # ResNet params
-                 as_vertices=False, sublevel=False, size=[28, 28], interval=[-1, 1], steps=32, hidden_features=[64, 32], scale=0.1):    # EC params
+                 as_vertices=False, sublevel=False, size=[28, 28], interval=[-2.5, 0], steps=32, hidden_features=[64, 32], scale=0.1):  # EC params
         super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
         self.gtheta_1 = GThetaEC(num_features=[32] + hidden_features)
         self.gtheta_2 = GThetaEC(num_features=[32] + hidden_features)
@@ -136,67 +134,25 @@ class ECResNet_Topo(ResNet18):
                         nn.ReLU(),
                         nn.Linear(64, num_classes))
         self.flatten = nn.Flatten()
-        self.tanh = nn.Tanh()
     
-    def forward(self, x):
-        x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
-        # ResNet
-        x = self.res_layer_1(x)
-
-        # insert ECLay after first residual layer, use tanh to scale between [-1,1]
-        x_2 = x.mean(dim=1, keepdim=True)
-        x_2 = self.topo_layer_3(self.tanh(x_2))
-
-        x = self.relu(x)
-        x = self.relu(self.res_layer_2(x))
-        x = self.relu(self.res_layer_3(x))
-        x = self.relu(self.res_layer_4(x))
-        x_1 = self.avg_pool(x)
-
-        # EC Layer 1
-        x_3 = self.gtheta_1(self.flatten(ecc_dtm005))
-
-        # EC Layer 2
-        x_4 = self.gtheta_2(self.flatten(ecc_dtm02))
-
-        x = torch.concat((x_1, x_2, x_3, x_4), dim=-1)
-        x = self.fc(x)
-        return x
-
-
-class ECResNet_Topo2(ResNet18):
-    def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,      # ResNet params
-                 as_vertices=False, sublevel=False, size=[14, 14], interval=[-1, 1], steps=32, hidden_features=[64, 32], scale=0.1):    # EC params
-        super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
-        self.gtheta_1 = GThetaEC(num_features=[32] + hidden_features)
-        self.gtheta_2 = GThetaEC(num_features=[32] + hidden_features)
-        self.topo_layer_3 = ECLay(as_vertices, sublevel, size, interval, steps, in_channels, hidden_features, scale=scale)
-        self.fc = nn.Sequential(nn.Linear(filter_cfg[-1] + 3*hidden_features[-1], 64),
-                        nn.ReLU(),
-                        nn.Linear(64, num_classes))
-        self.flatten = nn.Flatten()
-        self.tanh = nn.Tanh()
-
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
         # ResNet
         x = self.res_layer_1(x)
         x = self.res_layer_2(x)
 
-        # insert ECLay after second residual layer, use tanh to scale between [-1,1]
-        x_2 = x.mean(dim=1, keepdim=True)
-        x_2 = self.topo_layer_3(self.tanh(x_2))
+        x_2 = self.topo_layer_3(x.mean(dim=1, keepdim=True))    # insert ECLay after first second layer
         
         x = self.res_layer_3(x)
         x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
-
+        
         # EC Layer 1
         x_3 = self.gtheta_1(self.flatten(ecc_dtm005))
-
+        
         # EC Layer 2
         x_4 = self.gtheta_2(self.flatten(ecc_dtm02))
-
+        
         x = torch.concat((x_1, x_2, x_3, x_4), dim=-1)
         x = self.fc(x)
         return x
@@ -234,8 +190,8 @@ class PLResNet(ResNet18):
     
 
 class PLResNet_Topo(ResNet18):
-    def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,      # ResNet params
-                 as_vertices=False, sublevel=False, interval=[-7, 0], steps=32, K_max=2, dimensions=[0, 1], hidden_features=[64, 32]):  # PL params
+    def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,          # ResNet params
+                 as_vertices=False, sublevel=False, interval=[-2.5, 0], steps=32, K_max=2, dimensions=[0, 1], hidden_features=[64, 32]):    # PL params
         super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
         self.gtheta_1 = GThetaPL(num_features=[128] + hidden_features)
         self.gtheta_2 = GThetaPL(num_features=[128] + hidden_features)
@@ -249,9 +205,10 @@ class PLResNet_Topo(ResNet18):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
         # ResNet
         x = self.res_layer_1(x)
-        # insert ECLay after first residual layer
-        x_2 = self.topo_layer_3(x.mean(dim=1, keepdim=True))
         x = self.res_layer_2(x)
+
+        x_2 = self.topo_layer_3(x.mean(dim=1, keepdim=True))    # insert PLLay after second residual layer
+
         x = self.res_layer_3(x)
         x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
