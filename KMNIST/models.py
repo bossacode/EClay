919d7928a8516ccafd756a1270edc3cf62aa8257
bossacode <hle2745@gmail.com>
2024-05-18 05:26:50 +0900

    Use dtm with resnet

--- KMNIST/models.py
+++ KMNIST/models.py
@@ -39,16 +39,13 @@ class ResNet18(nn.Module):
             block (_type_, optional): Type of block to use. Defaults to ResidualBlock.
         """
         super().__init__()
-        assert len(block_cfg) == len(filter_cfg)
+        assert len(block_cfg) == len(filter_cfg) == 4
         self.res_in_channels = 1   # channel of input that goes into res_layer1, value changes in _make_layers
-        # self.conv_layer = nn.Sequential(nn.Conv2d(in_channels, self.res_in_channels, kernel_size=3, stride=2, padding=1),
-                                        # nn.BatchNorm2d(self.res_in_channels),
-                                        # nn.ReLU())
+
         self.res_layer_1 = self._make_layers(block, filter_cfg[0], block_cfg[0], stride=1)
         self.res_layer_2 = self._make_layers(block, filter_cfg[1], block_cfg[1], stride=2)
         self.res_layer_3 = self._make_layers(block, filter_cfg[2], block_cfg[2], stride=2)
         self.res_layer_4 = self._make_layers(block, filter_cfg[3], block_cfg[3], stride=2)
-        # self.res_layers = nn.Sequential(*[self._make_layers(block, num_filters, num_blocks, stride=(1 if i==0 else 2)) for i, (num_blocks, num_filters) in enumerate(zip(block_cfg, filter_cfg))])
         self.avg_pool = nn.Sequential(nn.AdaptiveAvgPool2d(1), nn.Flatten())
         self.fc = nn.Sequential(nn.Linear(filter_cfg[-1], 64),
                                 nn.ReLU(),
@@ -63,7 +60,6 @@ class ResNet18(nn.Module):
 
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
-        # x = self.conv_layer(x)
         x = self.res_layer_1(x)
         x = self.res_layer_2(x)
         x = self.res_layer_3(x)
@@ -110,59 +106,17 @@ class ECResNet(ResNet18):
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
         # ResNet
-        # x = self.conv_layer(x)
         x = self.res_layer_1(x)
         x = self.res_layer_2(x)
         x = self.res_layer_3(x)
         x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
-
-        # EC Layer 1
+        # ECC layer 1
         x_2 = self.gtheta_1(self.flatten(ecc_dtm005))
-
-        # EC Layer 2
+        # ECC layer 2
         x_3 = self.gtheta_2(self.flatten(ecc_dtm02))
-
+        # FC layer
         x = torch.concat((x_1, x_2, x_3), dim=-1)
-        x = self.fc(x)
-        return x
-    
-
-class ECResNet_Topo(ResNet18):
-    def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,      # ResNet params
-                 as_vertices=False, sublevel=False, size=[14, 14], interval=[-2.5, 0], steps=32, hidden_features=[64, 32], scale=0.1):  # EC params
-        super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
-        self.gtheta_1 = GThetaEC(num_features=[32] + hidden_features)
-        self.gtheta_2 = GThetaEC(num_features=[32] + hidden_features)
-        self.topo_layer_3 = ECLay(as_vertices, sublevel, size, interval, steps, in_channels, hidden_features, scale=scale)
-        self.fc = nn.Sequential(nn.Linear(filter_cfg[-1] + 3*hidden_features[-1], 64),
-                        nn.ReLU(),
-                        nn.Linear(64, num_classes))
-        self.flatten = nn.Flatten()
-    
-    def forward(self, x):
-        x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
-        # ResNet
-        # x = self.conv_layer(x)
-        x = self.res_layer_1(x)
-        
-        # insert ECLay after first res layer
-        x_2 = x.mean(dim=1, keepdim=True)
-        print(x_2.min().item(), x_2.max().item())
-        x_2 = self.topo_layer_3(x_2)
-
-        x = self.res_layer_2(x)        
-        x = self.res_layer_3(x)
-        x = self.res_layer_4(x)
-        x_1 = self.avg_pool(x)
-        
-        # EC Layer 1
-        x_3 = self.gtheta_1(self.flatten(ecc_dtm005))
-        
-        # EC Layer 2
-        x_4 = self.gtheta_2(self.flatten(ecc_dtm02))
-        
-        x = torch.concat((x_1, x_2, x_3, x_4), dim=-1)
         x = self.fc(x)
         return x
 
@@ -173,10 +127,11 @@ class PLResNet(ResNet18):
         super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
         self.gtheta_1 = GThetaPL(num_features=[128] + hidden_features)
         self.gtheta_2 = GThetaPL(num_features=[128] + hidden_features)
-        self.flatten = nn.Flatten()
         self.fc = nn.Sequential(nn.Linear(filter_cfg[-1] + 2*hidden_features[-1], 64),
                                 nn.ReLU(),
                                 nn.Linear(64, num_classes))
+        self.flatten = nn.Flatten()
+
     
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
@@ -186,14 +141,54 @@ class PLResNet(ResNet18):
         x = self.res_layer_3(x)
         x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
-
-        # Pl Layer 1
+        # PL layer 1
         x_2 = self.gtheta_1(self.flatten(pl_dtm005))
-
-        # PL Layer 2
+        # PL layer 2
         x_3 = self.gtheta_2(self.flatten(pl_dtm02))
-
+        # FC layer
         x = torch.concat((x_1, x_2, x_3), dim=-1)
+        x = self.fc(x)
+        return x
+
+
+class ECResNet_Topo(ResNet18):
+    def __init__(self, in_channels=1, num_classes=10, block_cfg=[2, 2, 2, 2], filter_cfg=[64, 128, 256, 512], block=ResidualBlock,      # ResNet params
+                 as_vertices=False, sublevel=False, size=[28, 28], interval=[-2.5, 0], steps=32, hidden_features=[64, 32], scale=0.1):  # EC params
+        super().__init__(in_channels, num_classes, block_cfg, filter_cfg, block)
+        self.gtheta_1 = GThetaEC(num_features=[32] + hidden_features)
+        self.gtheta_2 = GThetaEC(num_features=[32] + hidden_features)
+        self.topo_layer_3 = ECLay(as_vertices, sublevel, size, interval, steps, in_channels, hidden_features, scale=scale)
+        self.fc = nn.Sequential(nn.Linear(filter_cfg[-1] + 3*hidden_features[-1], 64),
+                        nn.ReLU(),
+                        nn.Linear(64, num_classes))
+        self.flatten = nn.Flatten()
+        self.dtm = WeightedDTMLayer(m0=0.05, lims=[[-0.5, 0.5], [-0.5, 0.5]], size=[28, 28])
+    
+    def forward(self, x):
+        x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
+        # ResNet
+        x = self.res_layer_1(x)
+
+        # insert ECLay after first res layer
+        x_2 = x.mean(dim=1, keepdim=True)
+        # if ((x_2 == 0).sum(dim=(2,3)) / (x.shape[2]*x.shape[3]) >= 0.90).any(): # to avoid dtm grad resulting in nan when input is very sparse
+        #     x_2 = self.topo_layer_3(x_2)
+        # else:
+        #     x_2 = self.dtm(x_2)
+        #     x_2 = self.topo_layer_3(x_2)
+        x_2 = self.dtm(x_2)
+        x_2 = self.topo_layer_3(x_2)
+
+        x = self.res_layer_2(x)        
+        x = self.res_layer_3(x)
+        x = self.res_layer_4(x)
+        x_1 = self.avg_pool(x)
+        # EC layer 1
+        x_3 = self.gtheta_1(self.flatten(ecc_dtm005))
+        # EC layer 2
+        x_4 = self.gtheta_2(self.flatten(ecc_dtm02))
+        # FC layer
+        x = torch.concat((x_1, x_2, x_3, x_4), dim=-1)
         x = self.fc(x)
         return x
     
@@ -205,10 +200,11 @@ class PLResNet_Topo(ResNet18):
         self.gtheta_1 = GThetaPL(num_features=[128] + hidden_features)
         self.gtheta_2 = GThetaPL(num_features=[128] + hidden_features)
         self.topo_layer_3 = PLLay(as_vertices, sublevel, interval, steps, K_max, dimensions, in_channels, hidden_features)
-        self.flatten = nn.Flatten()
         self.fc = nn.Sequential(nn.Linear(filter_cfg[-1] + 3*hidden_features[-1], 64),
                                 nn.ReLU(),
                                 nn.Linear(64, num_classes))
+        self.flatten = nn.Flatten()
+        self.dtm = WeightedDTMLayer(m0=0.05, lims=[[-0.5, 0.5], [-0.5, 0.5]], size=[28, 28])
     
     def forward(self, x):
         x, ecc_dtm005, ecc_dtm02, pl_dtm005, pl_dtm02 = x
@@ -217,20 +213,25 @@ class PLResNet_Topo(ResNet18):
 
         # insert PLLay after first res layer
         x_2 = x.mean(dim=1, keepdim=True)
-        print(x_2.min().item(), x_2.max().item())
+        # if ((x_2 == 0).sum(dim=(2,3)) / (x.shape[2]*x.shape[3]) >= 0.90).any(): # to avoid dtm grad resulting in nan when input is very sparse
+        #     print("no dtm", x_2.min().item(), x_2.max().item())
+        #     x_2 = self.topo_layer_3(x_2)
+        # else:
+        #     x_2 = self.dtm(x_2)
+        #     print("dtm", x_2.min().item(), x_2.max().item())
+        #     x_2 = self.topo_layer_3(x_2)
+        x_2 = self.dtm(x_2)
         x_2 = self.topo_layer_3(x_2)
 
         x = self.res_layer_2(x)
         x = self.res_layer_3(x)
         x = self.res_layer_4(x)
         x_1 = self.avg_pool(x)
-
-        # Pl Layer 1
+        # Pl layer 1
         x_3 = self.gtheta_1(self.flatten(pl_dtm005))
-
-        # PL Layer 2
+        # PL layer 2
         x_4 = self.gtheta_2(self.flatten(pl_dtm02))
-
+        # FC layer
         x = torch.concat((x_1, x_2, x_3, x_4), dim=-1)
         x = self.fc(x)
         return x
