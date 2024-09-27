c6f4ada5526d6c5a5fc796a221d938b3a27dd5a9
bossacode <hlee2745@gmail.com>
2024-09-27 20:14:40 +0900

    Change set_optimizer

--- MNIST-M/run_model.py
+++ MNIST-M/run_model.py
@@ -44,10 +44,12 @@ cfg["device"] = "cuda" if torch.cuda.is_available() else "cpu"
 def set_optimizer(model, cfg):
     param_list = []
     for name, layer in model.named_children():
-        if "res" in name or "fc" in name:   # set lr for ResNet layer and FC layer
-            param_list.append({"params": layer.parameters(), "lr": cfg["lr"]})
-        elif "ecc" in name:                 # set lr for all ECLayr
+        if "ecc" in name:   # set lr for all ECLayr
+            print("name: ",name, "lr: ", cfg["lr_topo"])
             param_list.append({"params": layer.parameters(), "lr": cfg["lr_topo"]})
+        else:
+            print("name: ",name, "lr: ", cfg["lr"])
+            param_list.append({"params": layer.parameters(), "lr": cfg["lr"]})
     optim = Adam(param_list, lr=cfg["lr"], weight_decay=0.0001)
     return optim
 
@@ -55,7 +57,7 @@ def set_optimizer(model, cfg):
 if __name__ == "__main__":
     nsim = 10                                       # number of simulations to run
     # train_size_list = [100, 300, 500, 700, 1000]    # training sample sizes
-    train_size_list = [1000]
+    train_size_list = [700]
     # cn_prob_list = [0.05, 0.1, 0.15, 0.2, 0.25]     # corruption and noise probabilities
     cn_prob_list = [0.15]
 
@@ -88,37 +90,38 @@ if __name__ == "__main__":
             model = models[args.model](**cfg["model_params"]).to(cfg["device"])
             # optim = Adam(model.parameters(), lr=cfg["lr"])
             optim = set_optimizer(model, cfg)
+            print(optim)
             train_test_wandb(model, cfg, optim, train_dl, val_dl, test_dl, weight_path, True, False, project, group, job_type, name)
             # train_test(model, cfg, optim, train_dl, val_dl, test_dl, weight_path)
 
 
     # loop over different noise probability
-    for p in cn_prob_list:
-        project = "MNIST_noise"     # used as project name in wandb
+    # for p in cn_prob_list:
+    #     project = "MNIST_noise"     # used as project name in wandb
 
-        print("-"*30)
-        print(f"Corruption & noise rate: {p}")
-        print("-"*30)
+    #     print("-"*30)
+    #     print(f"Corruption & noise rate: {p}")
+    #     print("-"*30)
 
-        prob = str(int(p * 100)).zfill(2)
-        data_dir = "./dataset/processed/cn_prob/"                       # base directory path to where data is loaded
-        weight_dir = f"./saved_weights/{args.model}/cn_prob/{prob}/"    # directory path to save trained weights
-        os.makedirs(weight_dir, exist_ok=True)
+    #     prob = str(int(p * 100)).zfill(2)
+    #     data_dir = "./dataset/processed/cn_prob/"                       # base directory path to where data is loaded
+    #     weight_dir = f"./saved_weights/{args.model}/cn_prob/{prob}/"    # directory path to save trained weights
+    #     os.makedirs(weight_dir, exist_ok=True)
         
-        # loop over number of simulations
-        for sim in range(1, nsim+1):
-            print(f"\nSimulation: [{sim} / {nsim}]")
-            print("-"*30)
+    #     # loop over number of simulations
+    #     for sim in range(1, nsim+1):
+    #         print(f"\nSimulation: [{sim} / {nsim}]")
+    #         print("-"*30)
             
-            weight_path = weight_dir + f"sim{sim}.pt"   # file path to save trained weights
-            group = args.model                          # used for grouping experiments in wandb
-            job_type = prob                             # used for grouping experiments in wandb
-            name = f"sim{sim}"                          # used for specifying runs in wandb
+    #         weight_path = weight_dir + f"sim{sim}.pt"   # file path to save trained weights
+    #         group = args.model                          # used for grouping experiments in wandb
+    #         job_type = prob                             # used for grouping experiments in wandb
+    #         name = f"sim{sim}"                          # used for specifying runs in wandb
         
-            train_dl, val_dl, test_dl = set_dataloader(data_dir + f"{prob}/train.pt", data_dir + f"{prob}/val.pt", data_dir + f"{prob}/test.pt", cfg["batch_size"])
+    #         train_dl, val_dl, test_dl = set_dataloader(data_dir + f"{prob}/train.pt", data_dir + f"{prob}/val.pt", data_dir + f"{prob}/test.pt", cfg["batch_size"])
 
-            model = models[args.model](**cfg["model_params"]).to(cfg["device"])
-            # optim = Adam(model.parameters(), lr=cfg["lr"])
-            optim = set_optimizer(model, cfg)
-            train_test_wandb(model, cfg, optim, train_dl, val_dl, test_dl, weight_path, True, False, project, group, job_type, name)
-            # train_test(model, cfg, optim, train_dl, val_dl, test_dl, weight_path)
+    #         model = models[args.model](**cfg["model_params"]).to(cfg["device"])
+    #         # optim = Adam(model.parameters(), lr=cfg["lr"])
+    #         optim = set_optimizer(model, cfg)
+    #         train_test_wandb(model, cfg, optim, train_dl, val_dl, test_dl, weight_path, True, False, project, group, job_type, name)
+    #         # train_test(model, cfg, optim, train_dl, val_dl, test_dl, weight_path)
--- MNIST/run_model.py
+++ MNIST/run_model.py
@@ -44,10 +44,12 @@ cfg["device"] = "cuda" if torch.cuda.is_available() else "cpu"
 def set_optimizer(model, cfg):
     param_list = []
     for name, layer in model.named_children():
-        if "res" in name or "fc" in name:   # set lr for ResNet layer and FC layer
-            param_list.append({"params": layer.parameters(), "lr": cfg["lr"]})
-        elif "ecc" in name:                 # set lr for all ECLayr
+        if "ecc" in name:   # set lr for all ECLayr
+            print("name: ",name, "lr: ", cfg["lr_topo"])
             param_list.append({"params": layer.parameters(), "lr": cfg["lr_topo"]})
+        else:
+            print("name: ",name, "lr: ", cfg["lr"])
+            param_list.append({"params": layer.parameters(), "lr": cfg["lr"]})
     optim = Adam(param_list, lr=cfg["lr"], weight_decay=0.0001)
     return optim
 
@@ -88,6 +90,7 @@ if __name__ == "__main__":
             model = models[args.model](**cfg["model_params"]).to(cfg["device"])
             # optim = Adam(model.parameters(), lr=cfg["lr"])
             optim = set_optimizer(model, cfg)
+            print(optim)
             train_test_wandb(model, cfg, optim, train_dl, val_dl, test_dl, weight_path, True, False, project, group, job_type, name)
             # train_test(model, cfg, optim, train_dl, val_dl, test_dl, weight_path)
 
