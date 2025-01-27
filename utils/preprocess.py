d2fd953c663fd11eb22372bc22129e8c62929998
bossacode <hlee2745@gmail.com>
2025-01-28 00:30:06 +0900

    Change so that noise is added indepently to each sample

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,34 +4,48 @@ from torch.utils.data import DataLoader
 from utils.dtm import WeightedDTMLayer, DTMLayer
 
 
+# add corruption and noise to MNIST data
 def corrupt_noise(x, p):
     """Replace pixels with random noise between 0 and 1 with probabilty p.
 
     Args:
-        x (torch.Tensor): Original data.
+        x (torch.Tensor): Original image data of shape [B, C, H, W]
         p (float): Corruption and noise probability.
 
     Returns:
         torch.Tensor: Data corrupted by random noise.
     """
-    dist = Bernoulli(probs=p)
-    x_crpt = torch.where(
-        dist.sample(x.shape).bool(),
-        0,
-        x)
-    x_crpt_noise = torch.where(
-        (x < 0.01) * dist.sample(x.shape).bool(),
-        torch.rand(x.shape),
-        x_crpt)
+    data_list = []
+    distr = Bernoulli(probs=p)
+    for data in x:
+        data_crpt = torch.where(distr.sample(data.shape).bool(),
+            0,
+            data)
+        data_crpt_noise = torch.where((data < 0.01) * distr.sample(data.shape).bool(),
+            torch.rand(data.shape),
+            data_crpt)
+        data_list.append(data_crpt_noise)
+    x_crpt_noise = torch.stack(data_list, dim=0)
     return x_crpt_noise
 
 
-def cn_transform(x, p):
-    data_list = []
-    dataloader = DataLoader(x, batch_size=64)
-    for data in dataloader:
-        data_list.append(corrupt_noise(data, p))
-    return torch.concat(data_list, dim=0)
+# add noise to ORBIT data
+def noise(x, p):
+    """Replace points with random noise between 0 and 1 with probabilty p.
+
+    Args:
+        x (torch.Tensor): One point cloud from original data. Shape [B, num_pts, D]
+        p (float): Noise probability.
+
+    Returns:
+        torch.Tensor: Data corrupted by random noise.
+    """
+    x_noise = x.clone()
+    distr = Bernoulli(probs=p)
+    for i in range(len(x)):
+        noise_ind, = distr.sample(sample_shape=(x.shape[1],)).nonzero(as_tuple=True)
+        x_noise[i, noise_ind, :] = torch.rand(len(noise_ind), x.shape[-1])
+    return x_noise
 
 
 def dtm_transform(x, m0, lims, size, weighted=True):
