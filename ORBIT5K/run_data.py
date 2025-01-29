bed1ae62dd3afb23826afd2b6b626b1ec6fe1667
bossacode <hlee2745@gmail.com>
2025-01-29 13:05:00 +0900

    Increase number of simulations

--- ORBIT5K/run_data.py
+++ ORBIT5K/run_data.py
@@ -37,8 +37,8 @@ cfg["device"] = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 if __name__ == "__main__":
-    nsim = 15                                           # number of simulations to run
-    num_orbits_list = [2000, 2500, 3000, 4000, 5000]    # number of generated orbits
+    nsim = 20                   # number of simulations to run
+    num_orbits_list = [5000]    # number of generated orbits
 
     wandb.login()
 
