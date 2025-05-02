0c946c4bfa64ce7676ba8b8c3895072787d99c42
bossacode <hlee2745@gmail.com>
2025-05-02 20:36:56 +0900

    Update code for runnning ablation study

--- MNIST/ablation.py
+++ MNIST/ablation.py
@@ -1,13 +1,9 @@
-import sys
-sys.path.append("../")
 import torch
-from torch.optim import Adam
-import os
 import wandb
 import argparse
 import yaml
-from utils.train import set_dataloader, train_test, train_test_wandb
-from models import Cnn, EcCnn_i, EcCnn
+from train_eval import run, run_wandb
+from models import Cnn, EcCnn_i, EcCnn, SigEcCnn
 
 
 # for reproducibility (may degrade performance)
@@ -17,54 +13,56 @@ torch.manual_seed(123)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("model", help="Name of model to train")
-parser.add_argument("param", help="")
+parser.add_argument("--model_flag", help="Name of model to train")
+parser.add_argument("--param", help="")
 args = parser.parse_args()
 
 
-models = {
+model_dict = {
     "Cnn": Cnn,
     "EcCnn_i": EcCnn_i,
-    "EcCnn": EcCnn
+    "EcCnn": EcCnn,
+    "SigEcCnn": SigEcCnn
     }
 
 
 # load configuration file needed for training model
-with open(f"ablation_configs/{args.model}{args.param}.yaml", "r") as f:
+with open(f"ablation_configs/{args.model_flag}{args.param}.yaml", "r") as f:
     cfg = yaml.load(f, yaml.FullLoader)
 cfg["device"] = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 if __name__ == "__main__":
     nsim = 15                   # number of simulations to run
-    train_size_list = [100, 1000]    # training sample sizes
+    # cn_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25]    # corruption and noise probabilities
+    cn_prob_list = [0.0]    # corruption and noise probabilities
 
     wandb.login()
+    project = "ablation"           # used as project name in wandb
+    group = args.model_flag     # used for grouping experiments in wandb
 
-    # loop over different training size
-    for train_size in train_size_list:
-        project = "test"      # used as project name in wandb
-        
+    # loop over different noise probability
+    for p in cn_prob_list:
+        prob = str(int(p * 100)).zfill(2)
+        job_type = prob                             # used for grouping experiments in wandb
+        data_dir = f"./dataset/processed/{prob}/"   # base directory path to where data is loaded
+
         print("-"*30)
-        print(f"Number of training data: {train_size}")
+        print(f"Corruption & noise rate: {p}")
         print("-"*30)
 
-        data_dir = "./dataset/processed/train_size/"                            # base directory path to where data is loaded
-        weight_dir = f"./saved_weights/{args.model}/train_size/{train_size}/"   # directory path to save trained weights
-        os.makedirs(weight_dir, exist_ok=True)
+        # weight_dir = f"./saved_weights/{args.model_flag}/{prob}/"    # directory path to save trained weights
+        # os.makedirs(weight_dir, exist_ok=True)
         
         # loop over number of simulations
         for sim in range(1, nsim+1):
             print(f"\nSimulation: [{sim} / {nsim}]")
             print("-"*30)
             
-            weight_path = weight_dir + f"sim{sim}.pt"   # file path to save trained weights
-            group = args.model                          # used for grouping experiments in wandb
-            job_type = str(train_size)                  # used for grouping experiments in wandb
-            name = f"sim{sim}"                          # used for specifying runs in wandb
+            # weight_path = weight_dir + f"sim{sim}.pt"   # file path to save trained weights
+            name = f"sim{sim}"  # used for specifying runs in wandb
 
-            train_dl, val_dl, test_dl = set_dataloader(data_dir + f"{train_size}/train.pt", data_dir + f"{train_size}/val.pt", data_dir + "test.pt", cfg["batch_size"])
-            model = models[args.model](**cfg["model_params"]).to(cfg["device"])
-            optim = Adam(model.parameters(), lr=cfg["lr"])
-            train_test_wandb(model, cfg, optim, train_dl, val_dl, test_dl, weight_path, True, False, project, group, job_type, name)
-            # train_test(model, cfg, optim, train_dl, val_dl, test_dl, weight_path)
+            model = model_dict[args.model_flag](**cfg["model_params"]).to(cfg["device"])
+            
+            run_wandb(model, cfg, data_dir, project, group, job_type, name)
+            # run(model, cfg, data_dir)
