5e6764afcafa428745452ae5854745927d391632
bossacode <hlee2745@gmail.com>
2025-05-02 18:53:05 +0900

    Fix code to run model

--- ph_models/run_orbit.py
+++ ph_models/run_orbit.py
@@ -3,7 +3,7 @@ import wandb
 import argparse
 import yaml
 from train_tf import run, run_wandb
-from ph_models.mnist_models import PersCnn, PlCnn_i, PlCnn
+from ph_models.orbit_models import PersCnn
 
 
 # for reproducibility (may degrade performance)
@@ -18,35 +18,35 @@ args = parser.parse_args()
 
 models = {
     "PersCnn": PersCnn,
-    "PlCnn_i": PlCnn_i,
-    "PlCnn": PlCnn
+    # "PlCnn_i": PlCnn_i,
+    # "PlCnn": PlCnn
     }
 
 
 # load configuration file needed for training model
-with open(f"configs/MNIST/{args.model_flag}.yaml", "r") as f:
+with open(f"configs/ORBIT5K/{args.model_flag}.yaml", "r") as f:
     cfg = yaml.load(f, yaml.FullLoader)
 
 
 if __name__ == "__main__":
     nsim = 20                                           # number of simulations to run
-    cn_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25]    # corruption and noise probabilities
+    noise_prob_list = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25] # noise probabilitieslities
 
     wandb.login()
-    project = "MNIST"           # used as project name in wandb
+    project = "ORBIT5K"         # used as project name in wandb
     group = args.model_flag     # used for grouping experiments in wandb
 
     # loop over different noise probability
-    for p in cn_prob_list:
+    for p in noise_prob_list:
         prob = str(int(p * 100)).zfill(2)
-        job_type = prob                                     # used for grouping experiments in wandb
-        data_dir = f"../MNIST/dataset/processed/{prob}/"    # base directory path to where data is loaded
+        job_type = prob                             # used for grouping experiments in wandb
+        data_dir = f"../ORBIT5K/dataset/{prob}/"    # base directory path to where data is loaded
 
         print("-"*30)
         print(f"Corruption & noise rate: {p}")
         print("-"*30)
-        
-        # weight_dir = f"./saved_weights/MNIST/{args.model_flag}/{prob}/"  # directory path to save trained weights
+
+        # weight_dir = f"./saved_weights/ORBIT5K/{args.model}/noise_prob/{prob}/" # directory path to save trained weights
         # os.makedirs(weight_dir, exist_ok=True)
         
         # loop over number of simulations
@@ -54,9 +54,9 @@ if __name__ == "__main__":
             print(f"\nSimulation: [{sim} / {nsim}]")
             print("-"*30)
             
-            # weight_path = weight_dir + f"sim{sim}.npz"   # file path to save trained weights
+            # weight_path = weight_dir + f"sim{sim}.npz"  # file path to save trained weights
             name = f"sim{sim}"                          # used for specifying runs in wandb
-            
+        
             model = models[args.model_flag](**cfg["model_params"])
             
             run_wandb(model, cfg, data_dir, project, group, job_type, name)
